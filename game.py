1576cb5b79260f16a5377532b379af313a0f1c91
ethanwc <ethanwc@u.washington.edu>
2019-04-08 12:03:18 -0700

    robust input handling

--- game.py
+++ game.py
@@ -1,30 +1,100 @@
 import sys
 
-searchMethods = {'BFS', 'DFS', 'DLS', 'ID', 'GBFS', 'AStar'}
+searchMethods = {'BFS', 'DFS', 'DLS', 'ID', 'GBFS', 'ASTAR'}
 heuristics = {'h1', 'h2'}
 
-print "Number of arguments: ", len(sys.argv)
 
-initialstate = sys.argv[1]
-searchMethod = sys.argv[2]
-print initialstate
-print len(initialstate)
-
-if len(initialstate) != 16: print "Invalid Input"
-
-check = True
-for c in initialstate:
-    if not str.isdigit(c) or (65 <= ord(c) <= 70):
-        check = False
-        print(check)
-        break
+# Breadth first search
+def bfs():
+    return 0
 
 
-def checkSearchMethod():
-    return searchMethod in searchMethods
+# Depth limited search
+def dfs():
+    return 0
 
 
-if searchMethod == 'BFS':
-elif searchMethod == 'DFS':
-elif searchMethod == 'DLS':
-print checkSearchMethod()
+# Load the cmd arguments into values
+def parseinput():
+    global initialstate, searchmethod, argcount, extra
+    initialstate = sys.argv[1].replace('\'', '').replace('\"', '').upper()
+    searchmethod = sys.argv[2]
+    argcount = len(sys.argv) - 1
+    input = ''
+    extra = ''
+
+    if argcount > 2:
+        extra = sys.argv[3]
+
+    for i in sys.argv[1:]:
+        input += i + ' '
+
+    print(input)
+
+
+# Check that the input is valid..
+def validateinput():
+    valid = True
+
+    if not searchmethod.upper() in searchMethods:
+        print('Invalid Search Method')
+        valid = False
+
+    if len(initialstate) != 16:
+        print("Invalid Initial state length: ", len(initialstate))
+        valid = False
+
+    # Check each char is between 0:9 and A:F
+    check = True
+    for c in initialstate:
+        if not (str.isdigit(c) or (65 <= ord(c) <= 70) or ord(c) == 32):
+            print('Invalid Initial State Characters')
+            print(c, ord(c))
+            check = False
+            break
+
+    if not check:
+        valid = False
+
+    # Special Cases for heuristics
+    if searchmethod.lower() == 'gbfs' or searchmethod.lower() == 'astar':
+        print('Invalid Input: No heuristic specified')
+        if not extra in heuristics:
+            valid = False
+
+    # Special Case for depth limited search
+    if searchmethod.lower() == 'dfs':
+        if extra in heuristics:
+            print('Invalid Input: Heuristic specified for depth')
+            valid = False
+        if argcount < 3:
+            print('Invalid Input: Depth length not specified?')
+            valid = False
+
+    # Should never be more than 3 input args
+    if argcount > 3:
+        print('Invalid Input: Too many arguments')
+        valid = False
+
+    if valid:
+        handleinput()
+
+
+# Handle input after checking it is okay.
+def handleinput():
+    if searchmethod.lower() == 'bfs':
+        print('bfs chosen')
+    elif searchmethod.lower() == 'dfs':
+        dfs()
+    elif searchmethod.lower() == 'dls':
+        print('dls chosen')
+    elif searchmethod.lower() == 'id':
+        print('id chosen')
+    elif searchmethod.lower() == 'gbfs':
+        print('gbfs chosen')
+    elif searchmethod.lower() == 'astar':
+        print('astar chosen')
+
+
+parseinput()
+validateinput()
