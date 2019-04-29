62624ab250354c416f585a907cb52b4769ea44cc
ethanwc <ethanwc@u.washington.edu>
2019-04-28 18:17:36 -0700

    done?

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+# ai moving tile problem by Ethan Cheatham
 import hashlib
 import sys
 import numpy as np
@@ -12,7 +13,6 @@ class PriorityQueue:
         self._size = 0
 
     def push(self, item, priority):
-        # print('p', priority)
         heapq.heappush(self._queue, (priority, self._index, item))
         self._index += 1
         self._size += 1
@@ -62,64 +62,45 @@ def h1(state):
 # the sum of the distances of the tiles from their goal positions
 # state should be 4x4
 def h2(state):
-    # print(state)
-    # print('--------------')
     distance = 0
     for x in range(0, size):
         for y in range(0, size):
             if state[x][y] == '1':
                 distance += abs(x - 0) + abs(y - 0)
-                # print(state[x][y],x,y, distance)
             if state[x][y] == '2':
                 distance += abs(x - 0) + abs(y - 1)
-                # print(state[x][y],x,y,distance)
             if state[x][y] == '3':
                 distance += abs(x - 0) + abs(y - 2)
-                # print(state[x][y],x,y,distance)
             if state[x][y] == '4':
                 distance += abs(x - 0) + abs(y - 3)
-                # print(state[x][y],x,y,distance)
             if state[x][y] == '5':
                 distance += abs(x - 1) + abs(y - 0)
-                # print(state[x][y],x,y,distance)
             if state[x][y] == '6':
                 distance += abs(x - 1) + abs(y - 1)
-                # print(state[x][y],x,y,distance)
             if state[x][y] == '7':
                 distance += abs(x - 1) + abs(y - 2)
-                # print(state[x][y],x,y,distance)
             if state[x][y] == '8':
                 distance += abs(x - 1) + abs(y - 3)
-                # print(state[x][y],x,y,distance)
             if state[x][y] == '9':
                 distance += abs(x - 2) + abs(y - 0)
-                # print(state[x][y],x,y,distance)
             if state[x][y] == 'A':
                 distance += abs(x - 2) + abs(y - 1)
-                # print(state[x][y],x,y,distance)
             if state[x][y] == 'B':
                 distance += abs(x - 2) + abs(y - 2)
-                # print(state[x][y],x,y,distance)
             if state[x][y] == 'C':
                 distance += abs(x - 2) + abs(y - 3)
-                # print(state[x][y],x,y,distance)
             if state[x][y] == 'D':
                 distance += abs(x - 3) + abs(y - 0)
-                # print(state[x][y],x,y,distance)
             if state[x][y] == 'E':
                 distance += abs(x - 3) + abs(y - 1)
-                # print(state[x][y],x,y,distance)
             if state[x][y] == 'F':
                 distance += abs(x - 3) + abs(y - 2)
-                # print(state[x][y],x,y,distance)
-    # print('--------------')
     return distance
 
 
 # pathcost g(n) should be the depth level, the number of moves taken, not the cost between initial and current.
 def pathcost(initial, current):
     n = 0
-    # print(initial, current)
     for i in range(0, len(initial)):
         if not initial[i] == current[i]:
             n += 1
@@ -131,7 +112,6 @@ def move(board, move):
     newboard = copy.deepcopy(board)
     for i in range(0, size):
         for j in range(0, size):
-            # print("board", board)
             if newboard[i][j] == ' ':
                 xoff = yoff = 0
                 if move == 'right':
@@ -147,7 +127,6 @@ def move(board, move):
                 newboard[i + yoff][j + xoff] = newboard[i][j]
                 newboard[i][j] = temp
                 return newboard
-    print('error')
     return newboard
 
 
@@ -155,7 +134,6 @@ def move(board, move):
 def getmoves(board):
     for i in range(0, size):
         for j in range(0, size):
-            # print(board)
             if board[i][j] == ' ':
                 moves = []
 
@@ -171,12 +149,12 @@ def getmoves(board):
                 # up
                 if i > 0:
                     moves.append('up')
-                # print(moves)
                 return moves
 
     return 0
 
 
+# check if a list contains an element
 def contains(element, list):
     length = len(list)
 
@@ -186,64 +164,7 @@ def contains(element, list):
     return False
 
 
-# bfs(0) or dfs(1) based on input, treats a list as a queue or stack, third param is the depth limit,
-def fs(initial, search, limit):
-    frontier = [initial]
-    explored = []
-
-    numcreated, numexpanded, maxfringe, depth, nextdepthincrease, depthincrease = 0, 0, 0, 0, 0, 1
-
-    # default want no limit, else keep default limit
-    if limit == 0:
-        limit = 10000000
-
-    maxdepth = limit
-
-    if np.array_equal(initial, solution):
-        return 0, 0, 0, 0
-
-    while len(frontier) > 0:
-        if len(frontier) > maxfringe:
-            maxfringe = len(frontier)
-            # bfs
-            if search:
-                node = frontier.pop(0)
-            # dfs
-            else:
-                node = frontier.pop()
-            print(node)
-        numexpanded += 1
-        explored.append(node)
-        moves = getmoves(node)
-        for i in range(0, len(moves)):
-            child = move(node, moves[i])
-            numcreated += 1
-            if not contains(child, explored):
-                if np.array_equal(child, solution2) or np.array_equal(child, solution):
-                    print(child)
-                    return depth, numcreated, numexpanded, maxfringe
-                frontier.append(child)
-
-            # if search:
-            #     depth += 1
-
-            # if search is bfs, count
-            # if not search:
-            nextdepthincrease += len(moves)
-            depthincrease -= 1
-            if depthincrease == 0:
-                depth += 1
-                if depth > maxdepth:
-                    return -1, -1, -1, -1
-                depthincrease = nextdepthincrease
-                nextdepthincrease = 0
-        # dfs just increment/decrement nodes
-        # else:
-        #     depth -= 1
-
-    return -1, -1, -1, -1
-
-
+# hash a string, best way considering space and time
 def md5(w):
     return hashlib.md5(w).hexdigest()[:9]
 
@@ -263,9 +184,6 @@ def bfs(initial):
     while frontier:
         location = next(iter(frontier))
         node = frontier.pop(location)
-        # print('****parent node****')
-        # print(node)
-        # print('*******************')
 
         if len(frontier) > maxfringe:
             maxfringe = len(frontier)
@@ -281,12 +199,9 @@ def bfs(initial):
             movecount += 1
             if pathhash not in frontier and pathhash not in explored:
                 created += 1
-                # print(child)
 
                 if np.array_equal(child, solution) or np.array_equal(child, solution2):
-                    print(child)
-                    print(currentDepth, created, expanded, maxfringe)
-                    exit()
+                    return currentDepth, created, expanded, maxfringe
 
                 frontier[pathhash] = child
 
@@ -294,12 +209,11 @@ def bfs(initial):
         elementsToDepthIncrease -= 1
         if elementsToDepthIncrease == 0:
             currentDepth += 1
-            print('depth level', currentDepth)
+            # print('depth level', currentDepth)
             if currentDepth > maxDepth:
                 return -1, -1, -1, -1
             elementsToDepthIncrease = nextElementsToDepthIncrease
             nextElementsToDepthIncrease = 0
-    print('no sol')
     return -1, -1, -1, -1
 
 
@@ -316,7 +230,6 @@ def dfs(initial, limit):
     if limit != 0:
         maxDepth = limit
 
-
     currentDepth = 1
     elementsToDepthIncrease = 1
     nextElementsToDepthIncrease = 0
@@ -328,31 +241,19 @@ def dfs(initial, limit):
             maxfringe = len(frontier)
 
         node = frontier.popitem()[1]
-
-        # print('****parent node****')
-        # print(node)
-        # print('*******************')
         hash = md5(np.array(node).tostring())
         explored[hash] = node
 
-        # 1234
-        # 5678
-        # A9BC
-        # DFE
-
         expanded += 1
         movecount = 0
-        # todo: reverse the paths
         for path in reversed(getmoves(node)):
             child = move(node, path)
             pathhash = md5(child.tostring())
             movecount += 1
             if pathhash not in frontier and pathhash not in explored:
                 created += 1
-                # print(child)
                 if np.array_equal(child, solution) or np.array_equal(child, solution2):
-                    print(currentDepth, created, expanded, maxfringe)
-                    exit()
+                    return currentDepth, created, expanded, maxfringe
 
                 frontier[pathhash] = child
 
@@ -362,8 +263,6 @@ def dfs(initial, limit):
             currentDepth += 1
             if currentDepth > maxDepth:
                 return -1, -1, -1, -1
-            else:
-                print('depth level', currentDepth)
             elementsToDepthIncrease = nextElementsToDepthIncrease
             nextElementsToDepthIncrease = 0
 
@@ -376,7 +275,6 @@ def hs(initial, search):
     frontier.push(initial, 1)
     explored = []
     if np.array_equal(initial, solution):
-        print('Found sol')
         return 0, 0, 0, 0
 
     maxDepth = 100000000
@@ -388,6 +286,7 @@ def hs(initial, search):
 
     while frontier.size() > 0:
         node = frontier.pop()
+        # print(node)
         explored.append(node)
         moves = getmoves(node)
         expanded += 1
@@ -502,7 +401,6 @@ def validateinput(initialstate, searchmethod, argcount, boardinput):
 # Handle input after checking it is okay.
 def handleinput(boardinput):
     res = 0
-    # print(boardinput)
     if searchmethod.lower() == 'bfs':
         res = bfs(boardinput)
     elif searchmethod.lower() == 'dfs':
@@ -519,6 +417,5 @@ def handleinput(boardinput):
         print(*res, sep=", ")
 
     # Start the program
-
 
 parseinput()
